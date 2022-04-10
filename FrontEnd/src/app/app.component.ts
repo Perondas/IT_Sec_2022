4967618e77895548bba640be25069fc9759f9732
Nikolaos_Bizbelis <117694@fhwn.ac.at>
2022-04-10 14:49:47 +0200

    small ui changes

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AuthenticationService } from './services/authentication/authentication.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FrontEnd';
+  constructor(public authService: AuthenticationService) {}
+
+  public onLogout() {
+    this.authService.logout();
+  }
 }
